6fca5c4b5004dbbf64e46d5068317413e6fe197b
dimitri.gedda <gratitudeadore@fastmail.com>
2021-06-16 22:51:09 -0400

    added Media Header Component

---
+++ SalesFloorDisplayFeature/media/MediaHeader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+mediaHeader: {
+
+  alignSelf: 'center',
+  letterSpacing: 2,
+  fontSize: 24,
+  fontWeight: '700',
+  textDecorationLine: 'underline',
+  color: 'tan',
+  marginBottom: 14,
+  marginTop: 45,
+}
+})
+
+const media = "Media"
+
+export const MediaHeader = () => {
+  return (
+    <Text style={styles.mediaHeader}>{`${media}`}</Text>
+  );
+};
