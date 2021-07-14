daa2a4752ffe884a363aca6fe664e1ca01f719f9
dimitri.gedda <gratitudeadore@fastmail.com>
2021-07-14 00:16:34 -0400

    zeroing commit tree

--- SalesFloorDisplayFeature/media-components/MediaContainer.js
+++ SalesFloorDisplayFeature/media-components/MediaContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { MediaHeader } from './MediaHeader'
 
 import { Media } from './Media'
