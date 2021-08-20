4d924da4529909674d99741d7d896d6a74db9cb9
DevD <gratitudeadore@fastmail.com>
2021-08-19 21:08:00 -0400

    added eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect",
+    }
+  }
+}
